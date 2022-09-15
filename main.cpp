374a96b547c82b3f043b89d4c0a6db73ae4b4933
D4niel <danielsirkia21@gmail.com>
2022-09-15 17:28:02 +0300

    Added Register/Login panel

--- main.cpp
+++ main.cpp
@@ -5,12 +5,14 @@
 #include <Windows.h>
 #include <stdio.h>
 int ans = 0;
-
+const char* dir = "c:\\Database.db";
 bool logs(std::string message, int type);
 std::string username;
 std::string password;
 static int createTable(const char* s);
 static int createDB(const char* s);
+static int callback(void* NotUsed, int argc, char** argv, char** azColName);
+static int insertData(const char* s, std::string sql);
 void SaveData_local(std::string message) {
 	std::ofstream SaveData_local;
 	SaveData_local.open("C://logs.txt");
@@ -80,12 +82,94 @@ void SendQuery(const char* s, std::string Query) {
 	}
 
 }
+bool Register() {
+	std::cout << "[*] Registration Panel";
+	color(8);
+	std::cout << "\n		Username:"; std::cin >> username; std::cout << "\n\n		Password:"; std::cin >> password;
+	if (username.length() < 3) {
+		logs("username length is too short", 2);
+		if (password.length() < 3) {
+			logs("password length is too short", 2);
+			return 1;
+		}
+		else {
+			logs("Saving to Database",1);
+			system("pause");
+			return 0;
+		}
+	}
+	else {
+		logs("Saving to Database", 1);
+		std::string save = "INSERT INTO User (username, password) VALUES ('" + username + "','" + password + "');";
+		insertData(dir, save);
+		logs("registration succesfull!",1);
+		system("pause");
+		return 0;
+	}
 
 
 
+}
+static int insertData(const char* s, std::string sql)
+{
+	sqlite3* DB;
+	char* messageError;
 
+
+
+	int exit = sqlite3_open(s, &DB);
+	/* An open database, SQL to be evaluated, Callback function, 1st argument to callback, Error msg written here */
+	exit = sqlite3_exec(DB, sql.c_str(), NULL, 0, &messageError);
+	if (exit != SQLITE_OK) {
+		std::cerr << "Error in insertData function." << std::endl;
+		sqlite3_free(messageError);
+	}
+	else
+		std::cout << "Records inserted Successfully!" << std::endl;
+
+	return 0;
+}
+static int selectData(const char* s, std::string sql)
+{
+	sqlite3* DB;
+	char* messageError;
+
+	
+
+	int exit = sqlite3_open(s, &DB);
+	/* An open database, SQL to be evaluated, Callback function, 1st argument to callback, Error msg written here*/
+	exit = sqlite3_exec(DB, sql.c_str(), callback, NULL, &messageError);
+
+	if (exit != SQLITE_OK) {
+		std::cerr << "Error in selectData function." << std::endl;
+		sqlite3_free(messageError);
+	}
+	else
+		std::cout << "Records selected Successfully!" << std::endl;
+
+	return 0;
+}
+
+bool Login() {
+	sqlite3* DB;
+	char* Error;
+	int exit = 0;
+	std::cout << "[*] Login Panel";
+	color(8);
+	std::cout << "\n		Username:"; std::cin >> username; std::cout << "\n\n		Password:"; std::cin >> password;
+	exit = selectData(dir, "SELECT * FROM User WHERE username = '" + username + "' AND password = '" + password + "';");
+	if (exit != SQLITE_OK) {
+		std::cout << "\nInvalid Credentials" << &Error;
+		sqlite3_free(Error);
+	}
+	else {
+	std::cout << "LOGIN SUCCESSFULL!";
+	}
+
+	return 0;
+}
 int main(const char* s) {
-	const char* dir = "c:\\Database.db";
+	
 	CheckForPrivilege();
 	sqlite3* db;
 	createDB(dir);
@@ -97,24 +181,33 @@ start:
 	std::cout << "[2] Login.\n\n";
 	std::cout << "[3] Exit.\n\n:";
 	std::cin >> ans;
+	if (ans == 2) {
+		Login();
+	}
+	if (ans == 1) {
+		Register();
+	
+	
+	}
 	if (ans == 0) {
 
 		char* error;
 		logs("Attempting to view Database", 1);
-		SendQuery(s, "SELECT * FROM User;");
-
+		selectData(dir, "SELECT * FROM User;");
+		system("pause");
 	}
 	if (ans == 3) {
 		system("exit");
 
 	}
 	if (ans == 1) {
-
+		system("cls");
+		Register();
 	}
 	else {
 		logs("Invalid Input", 2);
 		system("cls");
-
+		goto start;
 	}
 
 	return 0;
@@ -136,7 +229,7 @@ static int createDB(const char* s) {
 static int createTable(const char* s) {
 	int exception;
 	sqlite3* db;
-	std::string query = "CREATE TABLE IF NOT EXISTS User ("
+	std::string query = "CREATE TABLE User ("
 		"id INTEGER PRIMARY KEY AUTOINCREMENT,"
 		"username TEXT NOT NULL,"
 		"password TEXT NOT NULL,"
@@ -153,7 +246,7 @@ static int createTable(const char* s) {
 	else {
 		logs("User table Created Successfully", 1);
 		query = "CREATE TABLE IF NOT EXISTS ItemDB ("
-			"itemID INTEGER PRIMARY KEY AUTOINCREMENT,"
+			"itemID int PRIMARY KEY AUTOINCREMENT,"
 			"itemName TEXT NOT NULL);";
 		exit = sqlite3_open(s, &db);
 		exit = sqlite3_exec(db, query.c_str(), NULL, 0, &Error);
@@ -177,7 +270,7 @@ static int createTable(const char* s) {
 				logs("Marketplace Database table Created Successfully", 1);
 				query = "CREATE TABLE IF NOT EXISTS Inventory ("
 					"userID INTEGER PRIMARY KEY AUTOINCREMENT,"
-					"item INTEGER FOREIGN KEY REFERENCES ItemDB(itemID),"
+					"item int FOREIGN KEY REFERENCES ItemDB(itemID),"
 					"amount INTEGER);";
 					
 				exit = sqlite3_open(s, &db);
@@ -196,6 +289,17 @@ static int createTable(const char* s) {
 	}
 
 
+
+	return 0;
+}
+static int callback(void* NotUsed, int argc, char** argv, char** azColName)
+{
+	for (int i = 0; i < argc; i++) {
+		// column name and value
+		std::cout << azColName[i] << ": " << argv[i] << std::endl;
+	}
+
+	std::cout << std::endl;
 
 	return 0;
 }
