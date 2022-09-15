027d278ccdf9c16dbc634670d424326dce21825f
D4niel <danielsirkia21@gmail.com>
2022-09-15 16:25:33 +0300

    Massive Code Improve and nearly 1mb lighter executable.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include "sqlite/sqlite3.h"
+#include <sqlite3.h>
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -9,21 +9,23 @@ int ans = 0;
 bool logs(std::string message, int type);
 std::string username;
 std::string password;
-
+static int createTable(const char* s);
+static int createDB(const char* s);
 void SaveData_local(std::string message) {
 	std::ofstream SaveData_local;
 	SaveData_local.open("C://logs.txt");
 	if (SaveData_local.is_open()) {
-		logs("",2);
-}
+		logs("", 2);
+	}
 
 }
 
-void CheckForPrivilege() {
+bool CheckForPrivilege() {
 	std::ofstream savedata;
 	savedata.open("c://adminprivilegetest.txt");
 	if (savedata.is_open()) {
 		if (remove("c://adminprivilegetest.txt") == 0) {
+			return 0;
 		}
 	}
 	else
@@ -31,7 +33,9 @@ void CheckForPrivilege() {
 		perror("Admin privileges is required");
 		Sleep(2000);
 		system("exit");
+		return 1;
 	}
+
 }
 
 void color(int color) {
@@ -63,16 +67,16 @@ bool logs(std::string message, int type) { //prints out logs
 	return 0;
 }
 
-void SendQuery(std::string Query) {
-	char* err;
+void SendQuery(const char* s, std::string Query) {
+
 	sqlite3* db;
-	const sqlite3_stmt* stmt;
-	sqlite3_open("Kouluhomma.db", &db);
-	int a1 = sqlite3_exec(db, Query.c_str(), NULL,NULL, &err);
-	if (a1 != SQLITE_OK) {
-		logs("Query Request Failed. Reason:", 3);
-		std::cout << &err;
-	
+	char* Error;
+	int exit = 0;
+	exit = sqlite3_open(s, &db);
+	exit = sqlite3_exec(db, Query.c_str(), NULL, 0, &Error);
+	if (exit != SQLITE_OK) {
+		std::cout << "\nError While Creating Table:" << Error;
+		sqlite3_free(Error);
 	}
 
 }
@@ -80,21 +84,12 @@ void SendQuery(std::string Query) {
 
 
 
-int main() {
-	char* err;
-	sqlite3* db;
-	const sqlite3_stmt* stmt;
-	color(8);
+int main(const char* s) {
+	const char* dir = "c:\\Database.db";
 	CheckForPrivilege();
-	sqlite3_open("Kouluhomma.db", &db);
-	int a1 = sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS UserDB (id integer PRIMARY KEY, name varchar(20), password varchar(20));", NULL, NULL, &err);
-	int a2 = sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS ItemDB (id integer PRIMARY KEY, itemID varchar(20));", NULL, NULL, &err);
-	int a3 = sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS InventoryDB (id integer PRIMARY KEY, itemID integer FOREIGN KEY, amount integer)", NULL, NULL, &err);
-	int a4 = sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS MarketplaceDB (id integer PRIMARY KEY, price integer)",NULL,NULL, &err);
-	if (a1 || a2 || a3 || a4 != SQLITE_OK) {
-		logs("Creating Database Failed. Reason:", 3);
-		std::cout << &err;
-	}
+	sqlite3* db;
+	createDB(dir);
+	createTable(dir);
 
 start:
 	std::cout << "\n[0] View Database";
@@ -103,46 +98,63 @@ start:
 	std::cout << "[3] Exit.\n\n:";
 	std::cin >> ans;
 	if (ans == 0) {
-		std::string b2a = "SELECT * FROM UserDB";
-		int b2 = sqlite3_exec(db, b2a.c_str(), NULL, NULL, NULL);
-		if (b2 != SQLITE_OK) {
-			std::cout << &err;
-			
-		}
-		else {
-			system("cls");
-			std::cout << "\rOUTPUT:" <<b2;
-			system("pause");
-		}
+
+		char* error;
+		logs("Attempting to view Database", 1);
+		SendQuery(s, "SELECT * FROM User;");
+
 	}
 	if (ans == 3) {
 		system("exit");
-	
+
 	}
 	if (ans == 1) {
-		begin:
-			system("cls");
-			std::cout << "Enter Username:"; std::cin >> username;
-			std::cout << "\nEnter Password:"; std::cin >> password;
-			if (username.length() < 3) {
-				std::cout << "Username's length is not long enough. Expected Length (>3)";
-				goto begin;
-			}
-			if (password.length() < 3) {
-				std::cout << "Password's length is not long enough. Expected Length (>3)";
-				goto begin;
-			}
-			else {
-				username = username;
-				password = password;
-				SendQuery("INSERT INTO UserDB(name, password) VALUES(" + username + ", " + password + "");
-			}
+
 	}
 	else {
 		logs("Invalid Input", 2);
 		system("cls");
-		goto start;
+
 	}
+
+	return 0;
+}
+
+static int createDB(const char* s) {
+	sqlite3* db;
+	int exit = 0;
+	exit = sqlite3_open(s, &db);
+	if (exit != SQLITE_OK) {
+		std::cout << "Something went wrong..";
+	}
+	sqlite3_close(db);
+
+	return 0;
+
+}
+
+static int createTable(const char* s) {
+	int exception;
+	sqlite3* db;
+	std::string query = "CREATE TABLE IF NOT EXISTS User ("
+		"id INTEGER PRIMARY KEY AUTOINCREMENT,"
+		"username TEXT NOT NULL,"
+		"password TEXT NOT NULL,"
+		"cash INTEGER);";
+
+	char* Error;
+	int exit = 0;
+	exit = sqlite3_open(s, &db);
+	exit = sqlite3_exec(db, query.c_str(), NULL, 0, &Error);
+	if (exit != SQLITE_OK) {
+		std::cout << "\nError While Creating Table:" << Error;
+		sqlite3_free(Error);
+	}
+	else {
+		logs("Table Created Successfully", 1);
+	}
+
+
 
 	return 0;
 }
